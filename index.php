0e1651785c4d5a07dee90148214f2e5b8d012ede
prajakta s lavangare <psl9@afsconnect1.njit.edu>
2017-05-05 18:57:48 -0400

    Creating Add Todo Action Response

--- index.php
+++ index.php
@@ -16,10 +16,33 @@ if($action == NULL)
    $suc = isUserValid($username,$password);
    if($suc == true)
     {
-    echo 'Welcome';    
+    //echo 'Welcome';    
+    $result = getTodoItems($_COOKIE['my_id']);
+    include('list.php');
+    
     }else{
-    echo "Wrong User Name/Password!";
+    //echo "Wrong User Name/Password!";
+    
+    header("Location: badInfo.php");
     }
+    }else if ($action == 'registrar')
+     {
+        $name = filter_input(INPUT_POST, 'reg_uname');
+        if(isset($name))
+	{
+	$pass = filter_input(INPUT_POST, 'reg_password');
+	$exit = createUser($name,$pass);
+	if($exit == true)
+	{
+	include('user_exit.php');
+	}else {
+	header("Location: login.php");
+	}
+	}
+	}else if ($action == 'add')
+	{
+    
+    
     }
     ?>
   
